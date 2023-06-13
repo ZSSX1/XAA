6710bee7b83b2a63b1cfb50de67fee930baa4f87
KanekiOnRTX <132217266+KanekiOnRTX@users.noreply.github.com>
2023-06-13 22:05:22 +0400

    Update exynos-ufc.c

--- drivers/cpufreq/exynos-ufc.c
+++ drivers/cpufreq/exynos-ufc.c
@@ -32,7 +32,7 @@
  * Log2 of the number of scale size. The frequencies are scaled up or
  * down as the multiple of this number.
  */
-#define SCALE_SIZE           2
+#define SCALE_SIZE           3
 
 static int last_max_limit = -1;
 static int sse_mode;
